91bc2bae7001d4f750f6fb43589b3ba468999616
Julia Weis <julweis@users.noreply.github.com>
2016-11-07 22:23:29 -0600

    Version with Square, Square Root and Random

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,27 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+
+  def square
+
+    @square_input = params[:square_input]
+
+    @square = @square_input.to_i * @square_input.to_i
+    render("square.html.erb")
+  end
+
+  def square_root
+
+    @root_input = params[:root_input]
+
+    @root = Math.sqrt(@root_input.to_i)
+    render("square_root.html.erb")
+  end
+
+def random
+  @min = params[:min].to_i
+  @max = params[:max].to_i
+
+  @random = rand(@min..@max)
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+
+
+get("/square/:square_input", { :controller => "calculations", :action => "square" })
+
+get("/square_root/:root_input", { :controller => "calculations", :action => "square_root" })
+
+get("/random/:min/:max", { :controller => "calculations", :action => "random" })
+
 end
